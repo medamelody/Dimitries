071fef9763a72a7508669e3a39186fd1e151ee81
Lina-m31 <57687193+Lina-m31@users.noreply.github.com>
2026-06-08 14:37:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ if (yearElement) {
 
 const storageKey = 'pianist.social.links';
 const instagramAccountUrl = 'https://www.instagram.com/dhmhtrhs._pap?igsh=MXBtY256ejhrMGtrcA==';
-const YoutubeAccountUrl = 'https://www.facebook.com/profile.php?id=100008458355039';
+const facebookUrl = 'https://www.facebook.com/profile.php?id=100008458355039';
+const youtubeUri = 'https://www.youtube.com/@DimitriosPapakyriazis';
 
 const defaultLinks = [
   { id: crypto.randomUUID(), platform: 'Instagram', url: instagramAccountUrl },
-  { id: crypto.randomUUID(), platform: 'YouTube', url: YoutubeAccountUrl },
-  { id: crypto.randomUUID(), platform: 'Facebook', url: 'https://facebook.com/' },
+  { id: crypto.randomUUID(), platform: 'YouTube', url: youtubeUri },
+  { id: crypto.randomUUID(), platform: 'Facebook', url: facebookUrl },
 ];
 
 let socialLinks = loadLinks();
